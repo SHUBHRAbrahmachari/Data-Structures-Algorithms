0edbdadda574495ddfe2ac8b03ee4306b6fdd310
ShubhraBrahmachari <shubhrab2003@gmail.com>
2026-09-10 17:43:17 +0530

    fenwick tree bug resolved

---
+++ DynamicProgramming/SubsequenceDP/Q4_Largest_Non_Decreasing_Subsequence_Sum.cpp
@@ -0,0 +1,15 @@
+/*
+    Given an array of size N.
+    You are supposed to find the maximum possible subsequence sum such that the subsequence is a NON-DECREASING subsequence
+
+    1 <= N <= 10^5
+    -10^9 <= A[i] <= 10^9
+*/
+
+#include <iostream>
+#include <vector>
+#include "../../fenwick_tree.hpp"
+
+int main () {
+    
+}
--- fenwick_tree.hpp
+++ fenwick_tree.hpp
@@ -1,59 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 #include <stdexcept>
 
-template<typename T>
-class FenwickTree {
-    protected:
+template <typename T>
+class FenwickTreeSum {
+    private:
         size_t size;
         std::vector<T> tree;
+        std::vector<T> arr;
 
     public:
-
-        FenwickTree(const size_t size) {
+        FenwickTreeSum(const size_t size) {
             this->size = size;
+            this->arr.assign(this->size, 0);
             this->tree.assign(this->size+1, 0);
         }
 
-        FenwickTree(const std::vector<T>& array) {
-            this->size = array.size();
+        FenwickTreeSum(const std::vector<T>& arr) {
+            this->size = arr.size();
+            this->arr.assign(this->size, 0);
             this->tree.assign(this->size+1, 0);
-        }
 
-        virtual void update(const size_t index, const T delta) = 0;
-        virtual T query(const size_t index) = 0;
-};
-
-template<typename T>
-class FenwickTreeSum : public FenwickTree<T> {
-    public:
-
-        FenwickTreeSum(const size_t size) : FenwickTree<T>(size) {}
-
-        FenwickTreeSum(const std::vector<T>& array) : FenwickTree<T>(array) {
             for (size_t i=0; i<this->size; i++)
-                update(i, array[i]);
+                this->update(i, arr[i]);
         }
 
-        void update(const size_t index, const T new_value) override {
+        void update(const size_t index, const T new_value) {
             if (index >= this->size)
                 throw std::out_of_range("index out of range");
 
+            const T diff = new_value - this->arr[index];
+            this->arr[index] = new_value;
             size_t eff_index = index+1;
-            T delta = new_value - this->tree[eff_index];
 
             while (eff_index <= this->size) {
-                this->tree[eff_index] += delta;
+                this->tree[eff_index] += diff;
                 eff_index += (eff_index & -eff_index);
             }
         }
 
-        T query(const size_t index) override {
+        // sum of elements upto index `index` from index 0
+        T query(const size_t index) {
             if (index >= this->size)
                 throw std::out_of_range("index out of range");
 
-            T sum = 0;
             size_t eff_index = index+1;
+            T sum = 0;
+
             while (eff_index > 0) {
                 sum += this->tree[eff_index];
                 eff_index -= (eff_index & -eff_index);
