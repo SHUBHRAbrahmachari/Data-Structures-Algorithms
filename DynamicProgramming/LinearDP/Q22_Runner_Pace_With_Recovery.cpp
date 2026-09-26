254be15a4b2ac7a52f3377c2a08a5d8ce239dc7a
ShubhraBrahmachari <shubhrab2003@gmail.com>
2026-09-26 20:14:27 +0530

    DP - LinearDP - Runner Pace With Recovery updated with cleaner tabulation approach. RESTING CRITERIA YET TO BE REVIEWED

--- DynamicProgramming/LinearDP/Q22_Runner_Pace_With_Recovery.cpp
+++ DynamicProgramming/LinearDP/Q22_Runner_Pace_With_Recovery.cpp
@@ -10,55 +10,15 @@
     What is the maximum distance he can cover in that N minutes. The runner starts with initial energy K, and maximum energy can be K as well.
 
     1 <= |s[]| = |j[]| = |r[]| <= 10^5
-    1 <= s[i], j[i], r[i] <= 10^5
+    1 <= s[i], j[i], r[i] <= K <= 10^5
     1 <= J < S <= 10^5
-    1 <= K <= 100
 */
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-// O(NE)
-long long solve_memoization(
-    const std::vector<int>& s,
-    const std::vector<int>& j,
-    const std::vector<int>& r,
-    const int s_dis,
-    const int j_dis,
-    const int max_energy,
-    std::vector<std::vector<long long>>& memory,
-    const int index,
-    const int curr_energy
-) {
-    if (index >= s.size())
-        return 0;
-
-    if (memory[index][curr_energy] != -1)
-        return memory[index][curr_energy];
-
-    // case 1: the runner wants to rest this minute
-    long long option1 = solve_memoization(s, j, r, s_dis, j_dis, max_energy, memory, index+1, std::min(max_energy, curr_energy+r[index]));      // `curr_energy` must not exceed `max_energy`
-
-    // case 2: the runner wants to jog this minute
-    long long option2 = 0;
-    if (curr_energy - j[index] >= 0)
-        option2 = j_dis + solve_memoization(s, j, r, s_dis, j_dis, max_energy, memory, index+1, curr_energy-j[index]);
-
-    // case 3: the runner wants to sprint this minute
-    long long option3 = 0;
-    if (curr_energy - s[index] >= 0)
-        option3 = s_dis + solve_memoization(s, j, r, s_dis, j_dis, max_energy, memory, index+1, curr_energy-s[index]);
-
-    return std::max(
-        option1,
-        std::max(
-            option2,
-            option3
-        )
-    );
-}
-
-long long solve_tabulation(
+long long solve(
     const std::vector<int>& s,
     const std::vector<int>& j,
     const std::vector<int>& r,
@@ -68,45 +28,72 @@ long long solve_tabulation(
 ) {
     const int size = s.size();
 
-    // for all energy, at index=size distance has to be zero
+    /*
+        What is going to be our DP state?
 
-    // next DP represents index+1, curr_dp represents curr_index
-    std::vector<long long> next_dp(energy+1), curr_dp(energy+1);
+        memory[i][e]: with remaining energy e, what is the maximum distance the runner could cover upto i-th minute?
+    */
+    std::vector<std::vector<long long>> memory(
+        size,
+        std::vector<long long>(energy+1, -1)
+    );
 
-    // just reversing the parameter directions
-    for (int index=size-1; index>=0; index--) {
-        for (int e=0; e<=energy; e++) {
-            // option 1> rest this minute
-            int next_energy_rest = std::min(
-                energy,
-                e+r[index]
-            );
+    // upto 0-th minute if we're at same energy, we must have rested that minute
+    memory[0][energy] = 0;
 
-            long long option1 = next_dp[next_energy_rest];
-
-            // option 2> jog
-            long long option2 = 0;
-            if (e >= j[index])
-                option2 = j_dis + next_dp[e-j[index]];
-
-            // option 3> sprint
-            long long option3 = 0;
-            if (e >= s[index])
-                option3 = s_dis + next_dp[e-s[index]];
-
-            curr_dp[e] = std::max(
-                option1,
-                std::max(
-                    option2,
-                    option3
-                )
-            );
-        }
-
-        next_dp = curr_dp;
+    // in case jogging and sprinting requires same energy at 0-th minute
+    if (j[0] == s[0] and energy >= j[0])
+        memory[0][energy-j[0]] = std::max(
+            s_dis, j_dis
+        );
+    
+    // otherwise we need to set them differently 
+    else {
+        memory[0][energy-j[0]] = j_dis;
+        memory[0][energy-s[0]] = s_dis;
     }
 
-    return next_dp[energy];
+    for (int i=1; i<size; i++) {
+        for (int e=energy; e>=0; e--) {
+            // we just need to think, what were the ways that we could reach at this energy level!
+
+            /*
+                From which energy levels can we reach energy level e by resting?
+                Any valid energy state starting from e >= r[i] right?
+            */
+            long long option1 = -1;
+            if (e >= r[i]) {
+                for (int start = e-r[i]; start <= std::min(e, energy); start++)
+                    option1 = std::max(
+                        option1,
+                        memory[i-1][start]
+                    );
+            }
+
+            /*
+                From which energy levels can we reach energy level e by jogging?
+                We must have been in energy level e+j[i] to cut j[i] energy right? that means e+j[i] <= energy
+            */
+            long long option2 = -1;
+            if (e + j[i] <= energy and memory[i-1][e+j[i]] != -1)
+                option2 = memory[i-1][e+j[i]] + j_dis;
+
+            /*
+                From which energy levels can we reach energy level e by sprinting?
+                We must have been in energy level e+s[i] to cut s[i] energy right? that means e+s[i] <= energy
+            */
+            long long option3 = -1;
+            if (e + s[i] <= energy and memory[i-1][e+s[i]] != -1)
+                option3 = memory[i-1][e+s[i]] + s_dis;
+
+            memory[i][e] = std::max(
+                option1,
+                std::max(option2, option3)
+            );
+        }
+    }
+
+    return *std::max_element(memory[size-1].begin(), memory[size-1].end());
 }
 
 
@@ -135,8 +122,8 @@ int main() {
         )
     );
 
-    std::cout << "\nHe can cover atmost (using memoization approach)" << solve_memoization(s, j, r, sprint_distance, jog_distance, init_energy, memory, 0, init_energy) << " distance\n\n";
-    std::cout << "\nHe can cover atmost (using tabulation approach)" << solve_tabulation(s, j, r, sprint_distance, jog_distance, init_energy) << " distance\n\n";
-
+    const long long answer = solve(s, j, r, sprint_distance, jog_distance, init_energy);
+    std::cout << "\nHe can cover atmost " << answer << " distance\n\n";
+    
     return 0;
 }
